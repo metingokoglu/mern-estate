c57e1c6753e409806a912a86febe128991d88068
metingokoglu <metingkgl35@gmail.com>
2023-11-02 22:25:20 +0300

    complete sign up page ui

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,21 @@
-import React from "react";
+import {Link} from 'react-router-dom'
 
 export default function SignUp() {
-  return <div>SignUp</div>;
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7"> Hesap Oluştur</h1>
+      <form className="flex flex-col gap-4">
+        <input type="text" placeholder="Kullanıcı Adı" className="border p-3 rounded-lg" id="username"/>
+        <input type="text" placeholder="Email" className="border p-3 rounded-lg" id="email"/>
+        <input type="text" placeholder="Şifre" className="border p-3 rounded-lg" id="password"/>
+        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">Kayıt Ol</button>
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p>Hesabınız var mı?</p>
+        <Link to={"/sign-in"}>
+          <span className='text-blue-700'>Oturum Aç</span>
+        </Link>
+      </div>
+    </div>
+  )
 }
